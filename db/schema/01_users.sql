7a12daa6bbdf766745e9493e070dd90c9216835f
gary92gs <gary92.gs@gmail.com>
2024-02-11 07:57:24 -0700

    add users schema and seed for fedup db

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,11 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(11) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  super_user BOOLEAN NOT NULL DEFAULT FALSE
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,10 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+
+INSERT INTO users (username, phone_number, email, super_user)
+VALUES('KeyserSaucy', 2125550239, 'keyser@example.com');
+
+INSERT INTO users (username, phone_number, email) VALUES
+('QuipKingKint', 2125550187, 'verbal@example.com'),
+('NooseDodgerDean', 2125550192, 'dean@example.com'),
+('TriggerMike', 2125550223, 'michael@example.com'),
+('StammeringFenster', 2125550143, 'fred@example.com');
