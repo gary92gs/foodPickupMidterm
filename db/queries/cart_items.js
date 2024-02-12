32beef0693125d902496f19debdca322ad036ec6
vagrant <pihugupta0501@gmail.com>
2024-02-12 20:32:18 +0000

    Edit the functions to take the order id as param. Add the getCartItem functions to get a single cart item info associated with an order id

--- db/queries/cart_items.js
+++ db/queries/cart_items.js
@@ -4,10 +4,30 @@ const db = require('./../connection');
 //GET functions
 
 /**
-* @param {{user_id: integer}} userId
+* Browse all cart items for a user
+* @param {{orderId: integer}} orderId
 */
 
-const getCartItems = (userId) => {
+const getCartItems = (orderId) => {
+
+  return db.query(`
+  SELECT *
+  FROM cart_items
+  JOIN orders ON order_id = orders.id
+  WHERE order_id = $1;
+  `, [orderId])
+  .then((result) => {
+    return result.rows;
+  });
+
+};
+
+/**
+* Read a single cart item with cart item id
+* @param {{cart_item_id: integer}} cartItemId
+*/
+
+const getCartItem = (cartItemId) => {
 
   return db.query(`
   SELECT *
