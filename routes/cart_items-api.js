9750649330592796fbe456ba4621b7b2d81c02a9
vagrant <pihugupta0501@gmail.com>
2024-02-12 19:36:32 +0000

    Add the get route to browse cart items. Still have to extract the user from cookie and test

---
+++ routes/cart_items-api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const cartItemsQueries = require('../db/queries/cart_items');
+
+
+router.get('/', (req, res) => {
+
+  //extract user id through cookie
+
+  //get that user's order information
+  cartItemsQueries.getCartItems(userId)
+    .then(cartItems => {
+      res.json({ cartItems })
+    })
+    .catch(error => {
+      res.status(500).json({error: error.message});
+    })
+});
