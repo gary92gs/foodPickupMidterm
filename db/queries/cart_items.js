9c5ad6386d9fbe08e1f31b5a912ee69e569088a0
vagrant <pihugupta0501@gmail.com>
2024-02-12 19:29:14 +0000

    Modify functions to display cart items for a particular user with id

--- db/queries/cart_items.js
+++ db/queries/cart_items.js
@@ -2,19 +2,24 @@ const db = require('./../connection');
 
 
 //GET functions
-const getCartItems = () => {
+
+/**
+* @param {{user_id: integer}} userId
+*/
+
+const getCartItems = (userId) => {
 
   return db.query(`
   SELECT *
   FROM cart_items
-  JOIN orders ON order_id = orders.id;
-  `)
+  JOIN orders ON order_id = orders.id
+  WHERE orders.user_id = $1;
+  `, [userId])
   .then((result) => {
     return result.rows;
   });
 
 };
-
 
 
 
