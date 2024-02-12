654dbffaeeeddf7d85f48dbc1f7e29d005423aee
gary92gs <gary92.gs@gmail.com>
2024-02-12 15:16:16 -0700

    add shell for bread routes on menu-items-api

--- routes/menu-items-api.js
+++ routes/menu-items-api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const menuItemQueries = require('../db/queries/menu_itemsModel');
 
+//browse all menu_items
 router.get('/', (req, res) => {
   menuItemQueries.getMeals()
     .then(meals => {
@@ -12,6 +13,26 @@ router.get('/', (req, res) => {
         .status(500)
         .json({ error: err.message });
     });
+});
+
+//add individual meal_item
+router.post('/', (req, res) => {
+  res.send('inside /');
+});
+
+//read individual meal_item
+router.get('/:id', (req, res) => {
+  res.send('inside get user /:id');
+});
+
+//edit individual meal_item
+router.post('/:id', (req, res) => {
+  res.send('inside post user /:id');
+});
+
+//delete individual meal_item
+router.post('/:id/delete', (req, res) => {
+  res.send('inside post user /:id/delete');
 });
 
 module.exports = router;
