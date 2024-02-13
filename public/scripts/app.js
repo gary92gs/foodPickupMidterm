a7dbba51538705c50dd9d2c20c27b0a26cf464dd
Ryan Robertson <ryguyrobertson@gmail.com>
2024-02-12 17:24:55 -0800

    cleanup redundent helper and navheader component

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,25 +6,26 @@
 //   const $drinkBut = $('#drink-but');
 
 //   $appBut.on("click", function() {
-//     $appiesDiv =$('#appies');
+//     $appiesDiv =$('#Appies');
 //     const scrollTop = $appiesDiv.offset().top - 120;
 //     $('html, body').animate({ scrollTop: scrollTop });
 //   });
 
 //   $mainBut.on("click", function() {
-//     $appiesDiv =$('#mains');
+//     console.log("you clicked me teehee");
+//     $appiesDiv =$('#Mains');
 //     const scrollTop = $appiesDiv.offset().top - 120;
 //     $('html, body').animate({ scrollTop: scrollTop });
 //   });
 
 //   $desBut.on("click", function() {
-//     $appiesDiv =$('#desserts');
+//     $appiesDiv =$('#Desserts');
 //     const scrollTop = $appiesDiv.offset().top - 120;
 //     $('html, body').animate({ scrollTop: scrollTop });
 //   });
 
 //   $drinkBut.on("click", function() {
-//     $appiesDiv =$('#drinks');
+//     $appiesDiv =$('#Drinks');
 //     const scrollTop = $appiesDiv.offset().top - 120;
 //     $('html, body').animate({ scrollTop: scrollTop });
 //   });
