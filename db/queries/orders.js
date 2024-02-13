e90221ba60bc72b2f49432bcc90bc614121bc11f
vagrant <pihugupta0501@gmail.com>
2024-02-13 01:59:33 +0000

    Modify the add order function to also add the final cart items to the database received as an array of cart items through a promise

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -3,6 +3,7 @@ const db = require('./../connection');
 //Get functions
 /**
 * @param {{user_id: integer}} userId
+//join cart items and meal items
 */
 const getOrderWithUserId = (userId) => {
 
@@ -18,12 +19,17 @@ const getOrderWithUserId = (userId) => {
 
 };
 
+//get order by order id
+//can display name of customer
+//add ORDER BY;
+
 const getOrders = () => {
 
   return db.query(`
   SELECT *
   FROM orders
-  JOIN users ON user_id = users.id;
+  JOIN users ON user_id = users.id
+
   `)
   .then((result) => {
     return result.rows;
@@ -40,14 +46,38 @@ const getOrders = () => {
 
 const addOrder = (order) => {
 
+  let dbOrder = null;
+
   return db.query(`
   INSERT INTO orders (user_id, total_cost, placed_at, accepted_at, completed_at)
   VALUES ($1, $2, $3, $4, $5)
   RETURNING *;
   `,[order.user_id, order.total_cost, order.placed_at, order.accepted_at, order.completed_at])
   .then((result) => {
-    return result.rows;
+    dbOrder = result.rows[0];
+    const orderId = result.rows[0].id;
+    const promises = [];
+    for (const cartItem of order.cartItems) {
+
+    const promise = db.query(`
+    INSERT INTO cart_items (order_id, menu_item_id, quantity)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `,[orderId, cartItem.menu_item_id, cartItem.quantity])
+
+    promises.push(promise);
+    }
+    return Promise.all(promises);
+
+
+
+  })
+  .then((result) => {
+    console.log(result);
+    return dbOrder;
+    //order consfrimation page - or order is confirmed!
   });
+
 
 };
 
