2a45740992cf1079ea475edbb98e62e5d65eb3f6
vagrant <pihugupta0501@gmail.com>
2024-02-14 20:57:38 +0000

    Change button names to lower case

--- public/javascript/components/header.js
+++ public/javascript/components/header.js
@@ -11,10 +11,10 @@ $(() => {
           <p><i class="fa-solid fa-truck-fast"></i></p>
         </section>
         <section class="right-nav">
-          <button id="about-but" class="button-col" type="submit">ABOUT</button>
-          <button id="menu-but" class="button-col" type="submit">MENU</button>
-          <button id="cart-but" class="button-col" type="submit">CART</button>
-          <button type="submit">Order for Pickup</button>
+          <button id="about-but" class="button-col" type="submit">About</button>
+          <button id="menu-but" class="button-col" type="submit">Menu</button>
+          <button id="cart-but" class="button-col" type="submit">Cart</button>
+          <button id="orders-but" type="submit">Orders</button>
         </section>
       </nav>
     </div>
