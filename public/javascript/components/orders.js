d452c6f031dc26f9d46bc26c0d098506cff8b0e7
vagrant <pihugupta0501@gmail.com>
2024-02-14 21:23:26 +0000

    Add image to the page and apply image styles

--- public/javascript/components/orders.js
+++ public/javascript/components/orders.js
@@ -5,8 +5,9 @@ $(() => {
   function getOrdersPage() {
     menuItems.clear();
     let ordersHTML = `
-
-          <img src="/images/about.jpg">
+          <div id="orders-img-container">
+            <img src="/images/orders.jpg">
+          </div>
           <p> Welcome to The Culprit's Kitchen, an enigmatic haven in the heart of Vancouver, where culinary mastery meets cinematic intrigue. Inspired by the timeless allure of 'The Usual Suspects,' our high-end restaurant invites you to embark on a gastronomic journey like no other.
 
           Immerse yourself in an ambiance reminiscent of Keyser Söze's mysterious world, where our carefully curated menu unfolds a storyline through every dish. Our chefs, akin to directors, craft a symphony of flavors, while our attentive waitstaff play the roles of characters, ensuring your dining experience at The Culprit's Kitchen is nothing short of extraordinary. Indulge in rare wines, savor unexpected culinary revelations, and redefine your dining expectations with us. Welcome to a culinary experience where every meal is a masterpiece and every bite leaves you craving for more.<p>
